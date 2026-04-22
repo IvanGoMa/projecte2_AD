045c54838d4af0a2da157ade94b811810a230e2d
Hanna <hnnzpa@email.com>
2026-04-22 19:31:06 +0200

    Fetes els models: Address, Customer, Role y User

---
+++ project2/src/main/java/ivha/jpa/project2/Model/Role.java
@@ -0,0 +1,60 @@
+package ivha.jpa.project2.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name; // enum o check??
+    private String description;
+
+    public Role() {
+    }
+    public Role(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public List<User> getUsers() {
+        return users;
+    }
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+
+
+    // ManyToMany costat invers. 
+    @ManyToMany(mappedBy = "rols") // A user la llista es rols
+    private List<User> users = new ArrayList<>();
+    
+}
---
+++ project2/src/main/java/ivha/jpa/project2/Model/User.java
@@ -0,0 +1,97 @@
+package ivha.jpa.project2.Model;
+
+import java.security.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue 
+    private Long id;
+    @Column(nullable=false, length=100)
+    private String email;
+    @Column(length=20)
+    private String password;
+    private boolean status; 
+    private Timestamp dateCreated;
+    private Timestamp dateUpdated;
+    
+    
+    public User() {
+    }
+
+    //constructor
+    public User(String email, String password, boolean status, Timestamp dateCreated, Timestamp dateUpdated) {
+        this.email = email;
+        this.password = password;
+        this.status = status;
+        this.dateCreated = dateCreated;
+        this.dateUpdated = dateUpdated;
+    }
+
+    // getters i setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public boolean isStatus() {
+        return status;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    public Timestamp getDateCreated() {
+        return dateCreated;
+    }
+    public void setDateCreated(Timestamp dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+    public Timestamp getDateUpdated() {
+        return dateUpdated;
+    }
+    public void setDateUpdated(Timestamp dateUpdated) {
+        this.dateUpdated = dateUpdated;
+    }
+    
+    // ManyToMany - rol de usuari, en el costat del propietari
+    @ManyToMany
+    @JoinTable(
+        name = "user_roles", // taula auxiliar
+        joinColumns = @JoinColumn(name = "userId" ), // propietari
+        inverseJoinColumns = @JoinColumn(name = "roleId") // invers
+    )
+    private List<Role> rols = new ArrayList<>();
+
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Customer customer;
+}
